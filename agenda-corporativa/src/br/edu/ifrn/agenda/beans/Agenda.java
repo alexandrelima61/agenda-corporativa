c6e45e0ca1ecfb58935198726c1580337cc221a1
rafaeldias.a <rafaeldias.a@hotmail.com>
2009-12-17 21:37:51 +0000

    Bean da Agenda.

---
+++ agenda-corporativa/src/br/edu/ifrn/agenda/beans/Agenda.java
@@ -0,0 +1,49 @@
+package br.edu.ifrn.agenda.beans
+
+/**
+ *
+ * @author Rafael
+ */
+public class Agenda {
+
+    private int cod;
+    private String titulo;
+    private String descricao;
+    private boolean ativado = true;
+
+
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public boolean isAtivado() {
+        return ativado;
+    }
+
+    public void setAtivado(boolean ativado) {
+        this.ativado = ativado;
+    }
+
+    
+}
