8184495e8ea48cb4c2a44662e1d9fd9593e42cca
luizfelipe.smiranda@gmail.com <luizfelipe.smiranda@gmail.com@cea00fb0-e8e4-11de-a700-c3e9af1e83fa>
2009-12-17 21:47:47 +0000

    alterando a classe TarefaDAO para remover alguns erros.

--- trunk/agenda-corporativa/src/br/edu/ifrn/agenda/persistencia/TarefaDAO.java
+++ trunk/agenda-corporativa/src/br/edu/ifrn/agenda/persistencia/TarefaDAO.java
@@ -2,14 +2,21 @@ package br.edu.ifrn.agenda.persistencia;
 
 import java.sql.ResultSet;
 
+import br.edu.ifrn.agenda.beans.Tarefa;
+
 public class TarefaDAO {
 	
-	private Conexao conexao = Conexao.getInstance();
+	private Conexao conexao = null;
+	
+	public TarefaDAO(){
+		conexao = Conexao.getInstance();
+	}
 	
 	public Tarefa retornaTarefa(){
 		
 		ResultSet rs = conexao.executeQueryStatement("select * from ");
 		
+		return null;		
 	}
 	
 }
