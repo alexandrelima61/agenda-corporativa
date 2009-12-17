dbd3c6628e59bc66f26899a32e9dbd0825436343
rafaeldias.a@hotmail.com <rafaeldias.a@hotmail.com@cea00fb0-e8e4-11de-a700-c3e9af1e83fa>
2009-12-17 21:38:48 +0000

    DAO do caso de uso agenda.

---
+++ trunk/agenda-corporativa/src/br/edu/ifrn/agenda/persistencia/AgendaDAO.java
@@ -0,0 +1,102 @@
+package br.com.agenda.dao;
+
+import br.com.agenda.Agenda.Agenda;
+import br.com.agenda.util.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class AgendaDAO implements DAO {
+
+    private Connection conn;
+
+  
+    public AgendaDAO() throws Exception {
+        try {
+            this.conn = ConnectionFactory.getConnection();
+
+        } catch (Exception e) {
+            throw new Exception("Erro: " +
+                    "\n" + e.getMessage());
+        }
+    }
+
+
+    public void salvar(Agenda categoria) throws Exception {
+        PreparedStatement ps = null;
+        
+        if (categoria == null) {
+            throw new Exception("O valor passado não pode ser nulo");
+        }
+
+        try {
+            String SQL = "INSERT INTO tb_agenda (age_titulo, age_descricao, age_ativado) " +
+                    "values (?, ?, ?)";
+            ps = conn.prepareStatement(SQL);
+            ps.setString(1, categoria.getTitulo());
+            ps.setString(2, categoria.getDescricao());
+            ps.setBoolean(3, categoria.isAtivado());
+
+            ps.executeUpdate();
+
+        } catch (SQLException sqle) {
+            throw new Exception("Erro ao inserir dados " + sqle);
+        }
+    }
+
+    public List listarTodasAgendas() throws Exception{
+        PreparedStatement ps = null ;
+        List<Agenda> agendas = new ArrayList<Agenda>();
+            String SQL = "select * from tb_agenda";
+        try {
+            ps = conn.prepareStatement(SQL);
+            ResultSet rs = ps.executeQuery(SQL);
+            
+            while (rs.next()){
+                Agenda a = new Agenda();
+                a.setCod(rs.getInt("age_id"));
+                a.setTitulo(rs.getString("age_titulo"));
+                a.setDescricao(rs.getString("age_descricao"));
+                a.setAtivado(rs.getBoolean("age_ativado"));
+                agendas.add(a);
+            }
+        } catch (SQLException ex) {
+          throw new Exception("Erro ao pegar os dados " + ex);
+        } 
+        return agendas;
+    }
+
+    public void excluir(Agenda agenda) throws Exception {
+
+                PreparedStatement ps = null ;
+                try{
+                    String SQL = "DELETE from tb_agenda where age_id = ?";
+                    ps = conn.prepareStatement(SQL);
+                    ps.setInt(1, agenda.getCod());
+                    ps.executeUpdate();
+                } catch(Exception ex){
+                    throw new Exception("Erro ao deletar os dados " + ex);
+                }
+    }
+
+    public void editarAgenda(Agenda agenda) throws Exception {
+        PreparedStatement ps = null ;
+                try{
+                    String SQL = "UPDATE tb_agenda SET age_titulo = ?, age_descricao = ?," +
+                            "age_ativado = ? WHERE age_id = ?";
+                    ps = conn.prepareStatement(SQL);
+                    ps.setString(1, agenda.getTitulo());
+                    ps.setString(2, agenda.getDescricao());
+                    ps.setBoolean(3, agenda.isAtivado());
+                    ps.setInt(4, agenda.getCod());
+                    ps.executeUpdate();
+                } catch (SQLException ex){
+                    throw new Exception("Erro ao editar os dados " + ex);
+                }
+    }
+
+
+}
