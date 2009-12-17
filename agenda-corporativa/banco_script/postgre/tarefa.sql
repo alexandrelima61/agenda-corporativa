40ae5262797aa22855ceac1180cae2f37d28eebb
nda <luizfelipe.smiranda@gmail.com>
2009-12-17 21:40:25 +0000

    alterando o tipo do campo tar_data de datetime para timestamp no script postgre da tabela tarefa.

--- agenda-corporativa/banco_script/postgre/tarefa.sql
+++ agenda-corporativa/banco_script/postgre/tarefa.sql
@@ -3,7 +3,7 @@ CREATE TABLE tb_tarefa (
   age_id serial NOT NULL,
   usu_id serial NOT NULL,
   tar_titulo character varying(25),
-  tar_data datetime,
+  tar_data timestamp,
   tar_local character varying(40),
   tar_prioridade character varying(10),
   tar_descricao character varying(250),
