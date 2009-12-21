b9f37017e158823e1384ae5d786681dcd61f3002
leonardo.minora <leonardo.minora@cea00fb0-e8e4-11de-a700-c3e9af1e83fa>
2009-12-21 18:25:44 +0000

    modificacao do mecanismo de persistência.

--- agenda-corporativa/banco_script/postgre/lembrete.sql
+++ agenda-corporativa/banco_script/postgre/lembrete.sql
@@ -4,3 +4,11 @@ CREATE TABLE tb_lembrete (
   lem_corpo character varying(150),
   CONSTRAINT pk_lem_id PRIMARY KEY (lem_id)
 )
+
+CREATE TABLE tb_lembretes_datas (
+  lem_id serial NOT NULL,
+  dat_data date,
+  CONSTRAINT fk_lem_id FOREIGN KEY (lem_id)
+  	REFERENCES tb_lembrete (lem_id) MATCH SIMPLE
+  	ON UPDATE NO ACTION ON DELETE NO ACTION
+)
