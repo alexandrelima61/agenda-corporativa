6272292bebfd6658d5124fdbcac3e2ca0f5edd8a
cleilton19 <cleilton19@cea00fb0-e8e4-11de-a700-c3e9af1e83fa>
2009-12-21 17:24:17 +0000

    Inserindo arquivo responsável pela população da tabela tb_tarefa

---
+++ agenda-corporativa/banco_script/postgre/populacao_tarefa.sql
@@ -0,0 +1,36 @@
+INSERT INTO tb_tarefa(tar_id,age_id,usu_id,tar_titulo,tar_data,tar_local,tar_prioridade,tar_descricao,tar_estado,tar_ativado) 
+	VALUES (1,1,1,'Estudar p/ Prova de POO','2009-12-25','João Pessoa','alta','','aberto','true'); 
+
+
+INSERT INTO tb_tarefa(tar_id,age_id,usu_id,tar_titulo,tar_data,tar_local,tar_prioridade,tar_descricao,tar_estado,tar_ativado) 
+	VALUES (2,1,1,'Estudar p/ Prova de Redes','2009-12-25','João Pessoa','alta','','aberto','true'); 
+
+
+INSERT INTO tb_tarefa(tar_id,age_id,usu_id,tar_titulo,tar_data,tar_local,tar_prioridade,tar_descricao,tar_estado,tar_ativado) 
+	VALUES (3,1,1,'Limpar apartamento','2009-12-26','João Pessoa','baixa','','aberto','true'); 
+
+
+INSERT INTO tb_tarefa(tar_id,age_id,usu_id,tar_titulo,tar_data,tar_local,tar_prioridade,tar_descricao,tar_estado,tar_ativado) 
+	VALUES (4,1,1,'Lavar roupa','2009-12-27','Natal','media','','aberto','true'); 
+
+
+INSERT INTO tb_tarefa(tar_id,age_id,usu_id,tar_titulo,tar_data,tar_local,tar_prioridade,tar_descricao,tar_estado,tar_ativado) 
+	VALUES (5,1,1,'Elaborar ata de reunião da turma','2009-12-27','Natal','media','','aberto','true'); 
+	
+INSERT INTO tb_tarefa(tar_id,age_id,usu_id,tar_titulo,tar_data,tar_local,tar_prioridade,tar_descricao,tar_estado,tar_ativado) 
+	VALUES (6,1,1,'Finalizar trabalho de conclusão de curso','2010-1-7','Natal','alta','','aberto','true');
+	
+	
+INSERT INTO tb_tarefa(tar_id,age_id,usu_id,tar_titulo,tar_data,tar_local,tar_prioridade,tar_descricao,tar_estado,tar_ativado) 
+	VALUES (7,1,1,'Comprar pasagem para São Paulo','2010-1-15','Natal','media','','aberto','true');
+	
+INSERT INTO tb_tarefa(tar_id,age_id,usu_id,tar_titulo,tar_data,tar_local,tar_prioridade,tar_descricao,tar_estado,tar_ativado) 
+ 	VALUES (8,1,1,'Comprar roupas','2010-1-15','Natal','media','','aberto','true');
+	
+
+INSERT INTO tb_tarefa(tar_id,age_id,usu_id,tar_titulo,tar_data,tar_local,tar_prioridade,tar_descricao,tar_estado,tar_ativado) 
+	VALUES (9,1,1,'Preparar material para palestra','2010-1-16','Santa Cruz','alta','','aberto','true');
+	
+
+INSERT INTO tb_tarefa(tar_id,age_id,usu_id,tar_titulo,tar_data,tar_local,tar_prioridade,tar_descricao,tar_estado,tar_ativado) 
+	VALUES (10,1,1,'Sacar dinheiro no Banco','2010-1-17','Mossoró','media','','aberto','true');
