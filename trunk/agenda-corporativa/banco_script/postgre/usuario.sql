c96850c8ee20cbcdd65258074a0421a06c978025
leonardo.minora <leonardo.minora@cea00fb0-e8e4-11de-a700-c3e9af1e83fa>
2009-12-21 18:25:44 +0000

    modificacao do mecanismo de persistência.

--- trunk/agenda-corporativa/banco_script/postgre/usuario.sql
+++ trunk/agenda-corporativa/banco_script/postgre/usuario.sql
@@ -2,6 +2,8 @@ CREATE TABLE tb_usuario
 (
   usu_id serial NOT NULL,
   usu_nome character varying(60),
-  usu_login character varying(20),
-  CONSTRAINT pk_usu_id PRIMARY KEY (usu_id)
+  usu_login character varying(30),
+  usu_senha character varying(30),
+  usu_ativado boolean,
+  CONSTRAINT pk_usu_id PRIMARY KEY (usu_id)  
 )
