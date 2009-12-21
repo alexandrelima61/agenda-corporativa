61bfd203c70a376c9d444bdc0bf0f7983c996f88
leonardo.minora <leonardo.minora@cea00fb0-e8e4-11de-a700-c3e9af1e83fa>
2009-12-21 18:53:11 +0000

    teste e adaptação dos script dentro do PostgreSql

---
+++ agenda-corporativa/banco_script/postgre/tarefa-populacao.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_tarefa (age_id,usu_id,tar_titulo,tar_data,tar_local,tar_prioridade,tar_descricao,tar_estado,tar_ativado) 
+	VALUES (1,1,'Estudar p/ Prova de POO','2009-12-25','João Pessoa',1,'descricao',1,true);
+
+	
