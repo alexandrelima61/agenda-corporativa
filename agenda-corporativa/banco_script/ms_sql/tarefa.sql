0d2d1f6034a97aadddf8a4c98e05b5d64ea8766a
luizfelipe.smiranda <luizfelipe.smiranda@cea00fb0-e8e4-11de-a700-c3e9af1e83fa>
2009-12-21 19:29:36 +0000

    alteração do script de criação da tabela tarefa. spript testado!

--- agenda-corporativa/banco_script/ms_sql/tarefa.sql
+++ agenda-corporativa/banco_script/ms_sql/tarefa.sql
@@ -5,10 +5,10 @@ CREATE TABLE tb_tarefa (
 	tar_titulo varchar(25),
 	tar_data datetime,
 	tar_local varchar(40),
-	tar_prioridade varchar(10),
+	tar_prioridade int,
 	tar_descricao varchar(250),
-	tar_estado varchar(8),
-	tar_ativado boolean NOT NULL,
+	tar_estado int,
+	tar_ativado bit NOT NULL,
 	FOREIGN KEY(age_id) REFERENCES tb_agenda (age_id),
 	FOREIGN KEY(usu_id) REFERENCES tb_usuario (usu_id)
 )
