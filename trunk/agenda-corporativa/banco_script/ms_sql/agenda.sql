a58798df75242b1f17ba58287079c5bd4af5881b
leonardo.minora <leonardo.minora@cea00fb0-e8e4-11de-a700-c3e9af1e83fa>
2009-12-23 13:53:41 +0000

    adaptações finais nos scripts de criação das tabelas.
    Incluiu um script para criar uma agenda.

--- trunk/agenda-corporativa/banco_script/ms_sql/agenda.sql
+++ trunk/agenda-corporativa/banco_script/ms_sql/agenda.sql
@@ -1,7 +1,20 @@
-CREATE TABLE [dbo].[tb_agenda](
-	[age_id] [int] IDENTITY(1,1) NOT NULL,
-	[age_titulo] [varchar](50) NULL,
-	[age_descricao] [varchar](200) NULL,
-	[age_ativado] [bit] NULL,
- CONSTRAINT [PK_tb_agenda] PRIMARY KEY CLUSTERED 
-(
+CREATE TABLE tb_agenda(
+	age_id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	age_titulo varchar(50) NULL,
+	age_descricao varchar(200) NULL,
+	age_ativo bit NULL
+);
+
+
+CREATE TABLE tb_agenda_usuario (
+	age_usu_id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	age_id int NOT NULL,
+	usu_id int NOT NULL,
+
+	CONSTRAINT FK_tb_adenda_usuario_age
+		FOREIGN KEY (age_id)
+		REFERENCES tb_agenda (age_id),
+	CONSTRAINT FK_tb_adenda_usuario_usu
+		FOREIGN KEY (usu_id)
+		REFERENCES tb_usuario (usu_id)
+);
--- trunk/agenda-corporativa/banco_script/ms_sql/tarefa.sql
+++ trunk/agenda-corporativa/banco_script/ms_sql/tarefa.sql
@@ -1,14 +1,15 @@
 CREATE TABLE tb_tarefa (
 	tar_id int IDENTITY(1,1) PRIMARY KEY,
 	age_id int,
-	usu_id int,
-	tar_titulo varchar(25),
+	tar_titulo varchar(70),
 	tar_data datetime,
 	tar_local varchar(40),
 	tar_prioridade int,
 	tar_descricao varchar(250),
 	tar_estado int,
-	tar_ativado bit NOT NULL,
-	FOREIGN KEY(age_id) REFERENCES tb_agenda (age_id),
-	FOREIGN KEY(usu_id) REFERENCES tb_usuario (usu_id)
-)
+	tar_ativo bit NOT NULL,
+
+	CONSTRAINT FK_tb_tarefa_agen
+		FOREIGN KEY(age_id) 
+		REFERENCES tb_agenda (age_id)
+);
