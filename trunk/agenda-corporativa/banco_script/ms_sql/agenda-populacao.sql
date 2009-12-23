57e8970c9ca8de4f44b127b14816f7878ed031b4
rafaeldias.a@hotmail.com <rafaeldias.a@hotmail.com@cea00fb0-e8e4-11de-a700-c3e9af1e83fa>
2009-12-23 19:47:01 +0000

    populando.

--- trunk/agenda-corporativa/banco_script/ms_sql/agenda-populacao.sql
+++ trunk/agenda-corporativa/banco_script/ms_sql/agenda-populacao.sql
@@ -1,2 +1,6 @@
 INSERT INTO tb_agenda (age_titulo, age_descricao, age_ativo)
 	VALUES ('DIETINF', 'Diretoria de Educacao e Tecnologia da Informacao', 1);
+INSERT INTO tb_agenda (age_titulo, age_descricao, age_ativo)
+	VALUES ('Festas', 'Agenda de festas', 1);
+INSERT INTO tb_agenda (age_titulo, age_descricao, age_ativo)
+	VALUES ('Compromissos', 'Agenda de compromissos', 1);
