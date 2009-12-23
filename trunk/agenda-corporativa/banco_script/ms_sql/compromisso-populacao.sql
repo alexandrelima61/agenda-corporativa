8744bc2934a61ed30969bf13a156c0f87b98fc40
kairon.ramon <kairon.ramon@cea00fb0-e8e4-11de-a700-c3e9af1e83fa>
2009-12-23 18:48:52 +0000

    populacao da tabela compromisso.

---
+++ trunk/agenda-corporativa/banco_script/ms_sql/compromisso-populacao.sql
@@ -0,0 +1,18 @@
+Insert into tb_compromisso values(1, 2, 'Reunião DATINF', 'IFRN', 'Calendário 2010', 'true');
+Insert into tb_compromisso values(3, 1, 'Festa Fim de Ano', 'Hotel', 'Reveillon', 'true');
+Insert into tb_compromisso values(4, 1, 'Reunião escolar', 'CEI', 'Reajustes de mensalidade', 'true');
+Insert into tb_compromisso values(4, 1, 'Férias', 'Casa', 'Escolher destino da viagem', 'true');
+Insert into tb_compromisso values(1, 2, 'Provas finais', 'Lab 10', 'Marcar data', 'true');
+
+Insert into tb_compromisso_data values(1, '2009-01-11 09:00:00.000', '2009-02-11 11:00:00.000', 'true');
+Insert into tb_compromisso_data values(2, '2009-31-12 22:00:00.000', '2010-01-01 12:00:00.000', 'true');
+Insert into tb_compromisso_data values(3, '2010-12-01 08:00:00.000', '2010-12-01 12:00:00.000', 'true');
+Insert into tb_compromisso_data values(3, '2010-14-01 08:00:00.000', '2010-30-01 12:00:00.000', 'true');
+Insert into tb_compromisso_data values(3, '2010-02-01 08:00:00.000', '2010-02-01 12:00:00.000', 'true');
+
+
+Insert into tb_compromisso_participantes values(1, 2, 'true');
+Insert into tb_compromisso_participantes values(1, 3, 'true');
+Insert into tb_compromisso_participantes values(1, 4, 'true');
+Insert into tb_compromisso_participantes values(2, 4, 'true');
+Insert into tb_compromisso_participantes values(2, 3, 'true');
