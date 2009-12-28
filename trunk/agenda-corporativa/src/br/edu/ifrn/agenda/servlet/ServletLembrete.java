c8c47cd819f86a26bc60f05c18fa0786e0475918
bernardobfs <bernardobfs@cea00fb0-e8e4-11de-a700-c3e9af1e83fa>
2009-12-28 12:50:55 +0000

    Servlet modificado para a inserção de lembretes.

--- trunk/agenda-corporativa/src/br/edu/ifrn/agenda/servlet/ServletLembrete.java
+++ trunk/agenda-corporativa/src/br/edu/ifrn/agenda/servlet/ServletLembrete.java
@@ -9,40 +9,39 @@ import javax.servlet.http.HttpServletResponse;
 import br.edu.ifrn.agenda.beans.Lembrete;
 import br.edu.ifrn.agenda.persistencia.LembreteDAO;
 
-/**
- * Servlet implementation class ServletLembrete
- */
 public class ServletLembrete extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
     public ServletLembrete() {
-        // TODO Auto-generated constructor stub
+        
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String comando = request.getParameter("")
+		
 		String assunto = request.getParameter("assuntoLembrete");
 		String descricao = request.getParameter("descricaoLembrete");
 		String data = request.getParameter("dataLembrete");
+		String[] datas = data.split("/");
 		String hora = request.getParameter("dataLembrete");
+		String[] tempo = hora.split(":");
+		List<Date> listaDatas = new ArrayList<Date>();
+		Date data = new Date();
+		data.setDate(datas[0]);
+		data.setMonth(datas[1]);
+		data.setYear(datas[2]);
+		data.setHour(tempo[0]);
+		data.setMinute(tempo[1]);
+		listaDatas.add(data);
 		String[] alarme = request.getParameterValues("alarmarLembrete");
 		String periodicidade = request.getParameter("periodicidadeLembrete");
 		
-		//TODO Validar os dados necesarios á verificação
-		
-		//LembreteDAO dao = dao.inserirLembrete("assuntoLembrete", "descricaoLembrete", datas);
+		LembreteDAO dao = dao.inserirLembrete(assunto, descricao, listaDatas);
 	}
 
 }
