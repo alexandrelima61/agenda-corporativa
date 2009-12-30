32d4bf5daba775e048ba65a31611fab28728656e
matozito@bol.com.br <matozito@bol.com.br@cea00fb0-e8e4-11de-a700-c3e9af1e83fa>
2009-12-30 19:01:57 +0000

    Adaptado o servlet para a agenda. Modificação do comando inserirTarefa e editarTarefa para requisição post.

--- trunk/agenda-corporativa/src/br/edu/ifrn/agenda/servlet/TarefaServlet.java
+++ trunk/agenda-corporativa/src/br/edu/ifrn/agenda/servlet/TarefaServlet.java
@@ -1,6 +1,7 @@
 package br.edu.ifrn.agenda.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import com.sun.org.apache.xml.internal.serialize.Printer;
 
 import java.sql.Date;
 import java.sql.SQLException;
@@ -32,57 +35,8 @@ public class TarefaServlet extends HttpServlet {
 		String comando = request.getParameter("comando");
 		HttpSession session = request.getSession();
 		
-		if (comando.equalsIgnoreCase("inserirTarefa") || comando.equalsIgnoreCase("editarTarefa")){
-			
-			Tarefa tarefa = new Tarefa();
-			
-			String titulo = request.getParameter("tituloTarefa");
-			Prioridade prioridade = Prioridade.valueOf((request.getParameter("prioridadeTarefa").toUpperCase()));
-			Date data = Date.valueOf(request.getParameter("dataTarfa"));
-			String hora = request.getParameter("horaTarefa");
-			
-			String[] aux = hora.split(":");
-			data.setHours(Integer.parseInt(aux[0]));
-			data.setMinutes(Integer.parseInt(aux[1]));
-			
-			String local = request.getParameter("localTarefa");
-			String descricao = request.getParameter("descricao");
-			int oidAgenda = Integer.parseInt(request.getParameter("AgendaTarefa"));
-			
-			Agenda agenda = new Agenda();
-			agenda.setOid(oidAgenda);
-			
-			Usuario usuario = (Usuario) session.getAttribute("usuario");
-			
-			tarefa.setUsuario(usuario);
-			tarefa.setAgenda(agenda);
-			tarefa.setTitulo(titulo);
-			tarefa.setPrioridade(prioridade);
-			tarefa.setEstado(EstadoTarefa.ABERTO);
-			tarefa.setData(data);
-			tarefa.setLocal(local);
-			tarefa.setDescricao(descricao);
-			tarefa.setAtivo(true);
-			
-			if (comando.equalsIgnoreCase("inserirTarefa"))
-				try {
-					
-					new TarefaDAO().inserir(tarefa);
-					
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			else{
-				int oidTarefa = Integer.parseInt(request.getParameter("oidTarefa"));
-				tarefa.setOid(oidTarefa);
-				new TarefaDAO().salvar(tarefa);
-			}
-			
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/* caminho pagina destino */");
-			dispatcher.forward(request, response);
-		}
 		
-		else if(comando.equalsIgnoreCase("removerTarefa")){
+		if(comando.equalsIgnoreCase("removerTarefa")){
 
 			int oidTarefa = Integer.parseInt(request.getParameter("oidTarefa"));
 			
@@ -112,7 +66,63 @@ public class TarefaServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		
+		String comando = request.getParameter("comando");
+		HttpSession session = request.getSession();
+		
+		if (comando.equalsIgnoreCase("inserirTarefa") || comando.equalsIgnoreCase("editarTarefa")){
+			
+			Tarefa tarefa = new Tarefa();
+			
+			String titulo = request.getParameter("tituloTarefa");
+			Prioridade prioridade = Prioridade.valueOf((request.getParameter("prioridadeTarefa").toUpperCase()));
+			Date data = Date.valueOf(request.getParameter("dataTarefa"));
+			String hora = request.getParameter("horaTarefa");
+			
+			String[] aux = hora.split(":");
+			data.setHours(Integer.parseInt(aux[0]));
+			data.setMinutes(Integer.parseInt(aux[1]));
+			
+			String local = request.getParameter("localTarefa");
+			String descricao = request.getParameter("descricaoTarefa");
+			int oidAgenda = Integer.parseInt(request.getParameter("AgendaTarefa"));
+			
+			Agenda agenda = new Agenda();
+			agenda.setOid(oidAgenda);
+			
+			Usuario usuario = (Usuario) session.getAttribute("usuario");
+			
+			tarefa.setUsuario(usuario);
+			tarefa.setAgenda(agenda);
+			tarefa.setTitulo(titulo);
+			tarefa.setPrioridade(prioridade);
+			tarefa.setEstado(EstadoTarefa.ABERTO);
+			tarefa.setData(data);
+			tarefa.setLocal(local);
+			tarefa.setDescricao(descricao);
+			tarefa.setAtivo(true);
+			
+			if (comando.equalsIgnoreCase("inserirTarefa"))
+				try {
+					
+					new TarefaDAO().inserir(tarefa);
+					PrintWriter pw = response.getWriter();
+					pw.print("ok");
+					pw.flush(); pw.close();
+					
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			else{
+				int oidTarefa = Integer.parseInt(request.getParameter("oidTarefa"));
+				tarefa.setOid(oidTarefa);
+				new TarefaDAO().salvar(tarefa);
+			}
+			
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/* caminho pagina destino */");
+			dispatcher.forward(request, response);
+		}
+		
 	}
 
 }
