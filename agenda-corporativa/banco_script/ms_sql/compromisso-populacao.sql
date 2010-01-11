877f1271073c8bc912ef2cc033a64b35cf46c8b0
henriquecolorado <henriquecolorado@cea00fb0-e8e4-11de-a700-c3e9af1e83fa>
2010-01-11 13:03:19 +0000

    Correção da população da tabela:  tb_compromisso_data

--- agenda-corporativa/banco_script/ms_sql/compromisso-populacao.sql
+++ agenda-corporativa/banco_script/ms_sql/compromisso-populacao.sql
@@ -4,11 +4,9 @@ Insert into tb_compromisso (usu_id, age_id, com_titulo, com_local, com_descricao
 Insert into tb_compromisso (usu_id, age_id, com_titulo, com_local, com_descricao, com_ativo) values(1, 2, 'Provas finais', 'Lab 10', 'Marcar data', 'true');
 
 Insert into tb_compromisso_data (com_id, com_dat_datainicio, com_dat_datafim, com_dat_ativo) values(1, '2010-12-01 08:00:00.000', '2010-12-01 12:00:00.000', 'true');
-Insert into tb_compromisso_data (com_id, com_dat_datainicio, com_dat_datafim, com_dat_ativo) values(1, '2009-13-01 09:00:00.000', '2009-01-11 11:00:00.000', 'true');
-Insert into tb_compromisso_data (com_id, com_dat_datainicio, com_dat_datafim, com_dat_ativo) values(1, '2010-14-01 08:00:00.000', '2010-14-01 12:00:00.000', 'true');
 Insert into tb_compromisso_data (com_id, com_dat_datainicio, com_dat_datafim, com_dat_ativo) values(1, '2010-02-01 08:00:00.000', '2010-02-01 12:00:00.000', 'true');
-Insert into tb_compromisso_data (com_id, com_dat_datainicio, com_dat_datafim, com_dat_ativo) values(2, '2009-31-12 22:00:00.000', '2009-31-12 12:00:00.000', 'true');
-Insert into tb_compromisso_data (com_id, com_dat_datainicio, com_dat_datafim, com_dat_ativo) values(3, '2009-20-01 14:00:00.000', '2009-20-01 16:00:00.000', 'true');
+Insert into tb_compromisso_data (com_id, com_dat_datainicio, com_dat_datafim, com_dat_ativo) values(2, '2009-12-31 22:00:00.000', '2009-12-31 12:00:00.000', 'true');
+Insert into tb_compromisso_data (com_id, com_dat_datainicio, com_dat_datafim, com_dat_ativo) values(3, '2009-01-20 14:00:00.000', '2009-01-20 16:00:00.000', 'true');
 Insert into tb_compromisso_data (com_id, com_dat_datainicio, com_dat_datafim, com_dat_ativo) values(4, '2009-05-01 14:00:00.000', '2009-05-01 16:00:00.000', 'true');
 
 Insert into tb_compromisso_participantes (com_id, usu_id) values(1, 2);
