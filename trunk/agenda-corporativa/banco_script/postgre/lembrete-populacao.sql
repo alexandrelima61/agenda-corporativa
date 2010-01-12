fb3f1d87571a3a5a72037b49f3e8d0c880a099e2
cordus88 <cordus88@cea00fb0-e8e4-11de-a700-c3e9af1e83fa>
2010-01-12 12:49:11 +0000

    Adicionar lembrete-populacao para postgres

---
+++ trunk/agenda-corporativa/banco_script/postgre/lembrete-populacao.sql
@@ -0,0 +1,23 @@
+INSERT INTO tb_lembrete (age_id,lem_titulo,lem_corpo,lem_ativo) 
+	VALUES (1,'Medicamento','Tomar Medicamente Previsto',true);
+
+INSERT INTO tb_lembrete (age_id,lem_titulo,lem_corpo,lem_ativo) 
+	VALUES (1,'Aviso','Avisar superior sobre recado da esposa',true);
+
+INSERT INTO tb_lembrete (age_id,lem_titulo,lem_corpo,lem_ativo) 
+	VALUES (1,'Natal','Lembrar do nascimento do menino Jesus',true);
+
+INSERT INTO tb_lembretes_datas(lem_id,lem_dat_data) 
+	VALUES (1,'2009-12-24 06:00:00');
+
+INSERT INTO tb_lembretes_datas(lem_id,lem_dat_data) 
+	VALUES (1,'2009-12-24 12:00:00');
+	
+INSERT INTO tb_lembretes_datas(lem_id,lem_dat_data) 
+	VALUES (2,'2009-12-24 18:00:00');
+
+INSERT INTO tb_lembretes_datas(lem_id,lem_dat_data) 
+	VALUES (2,'2009-12-24 14:00:00');
+
+INSERT INTO tb_lembretes_datas(lem_id,lem_dat_data) 
+	VALUES (3,'2009-12-25 00:00:00');
